2e3b3cce1ea68f8726ab95b6b5ebcfbbf9a2b8aa
Vincent Verges <me@vincentverges.com>
2024-09-01 22:54:25 +0200

    update config

--- parking_management/settings.py
+++ parking_management/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'parking_management.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'NAME': os.getenv('POSTGRES_DB','parking_db'),
+        'USER': os.getenv('POSTGRES_USER','postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD','postgres'),
         'HOST': os.getenv('POSTGRES_HOST','localhost'),
         'PORT': os.getenv('POSTGRES_PORT','5432'),
     }
